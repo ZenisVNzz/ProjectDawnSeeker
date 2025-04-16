dd92ed0c1efe007d256e67904b968d00be62d740
ZenisVNzz <phuoc0948@gmail.com>
2025-04-16 15:27:33 +0700

    Add CharacterData, CharacterInBattle script

---
+++ Assets/Scripts/Entity/Data/CharacterData.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public enum characterType { Player, Enemy }
+
+[CreateAssetMenu(fileName = "Character", menuName = "Characters/NewCharacter")]
+public class CharacterData : ScriptableObject
+{
+    public int characterID;
+    public string characterName;
+    public characterType characterType;
+    public Sprite characterSprite;
+    public Animator animator;
+    public float ATK;
+    public float HP;
+    public float DEF;
+    public float MP;
+    public float CR;
+    public float CD;
+    public float DC;
+    public float PC;
+}
---
+++ Assets/Scripts/Entity/InBattle/CharacterInBattle.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+public class CharacterInBattle : MonoBehaviour
+{
+    public CharacterData characterData {  get; private set; }
+    public string charName {  get; private set; }
+    public float ATK { get; private set; }
+    public float HP { get; private set; }
+    public float DEF { get; private set; }
+    public float MP { get; private set; }
+    public float CR { get; private set; }
+    public float CD { get; private set; }
+    public float DC { get; private set; }
+    public float PC { get; private set; }
+    public bool isAlive = true;
+
+    public event Action OnDeath;
+
+    public CharacterInBattle(CharacterData characterData)
+    {
+        this.characterData = characterData;
+        this.charName = characterData.characterName;
+        this.ATK = characterData.ATK;
+        this.HP = characterData.HP;
+        this.DEF = characterData.DEF;
+        this.MP = characterData.MP;
+        this.CR = characterData.CR;
+        this.CD = characterData.CD;
+        this.DC = characterData.DC;
+        this.PC = characterData.PC;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        HP -= damage;
+        if (HP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        HP += healAmount;
+        if (HP > characterData.HP)
+        {
+            HP = characterData.HP;
+        }
+    }
+    
+    public void Die()
+    {
+        OnDeath?.Invoke();
+    }
+}
