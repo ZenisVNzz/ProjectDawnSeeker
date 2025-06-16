30af284da39e8884bc04e073dbd93c111786e661
ZenisVNzz <phuoc0948@gmail.com>
2025-06-16 09:13:37 +0700

    Fixed stun

--- Assets/Scripts/Entity/Skills/Knight/Bash.cs
+++ Assets/Scripts/Entity/Skills/Knight/Bash.cs
@@ -24,7 +24,6 @@ public class Bash : SkillBase
         {
             provoke.provocateur = user;
             target.ApplyStatusEffect(provoke, 0);
-            user.ApplyStatusEffect(defUP, 0);
         }
     }
 
